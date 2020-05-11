ce59dd84085a9a7b8e18eb429f45a92991bd01aa
malcolm staso <mstaso1@gmail.com>
2020-05-11 22:18:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,10 @@ row_index = 0
 smallest_numbers = []
 while row_index < src.count do
   element_index = 0
-  shortest_string_element = ""
+  smallest_integer = []
   while element_index < src[row_index].count do
-    if src[row_index][element_index].length < shortest_string_element.length
-      shortest_string_element = src[row_index][element_index]
+    if src[row_index][element_index].length < smallest_integer.length
+      smallest_integer = src[row_index][element_index]
     end
     element_index += 1
   end
